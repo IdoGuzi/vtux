d94a27645d72e33e98c10e0fe8d2487381f729c5
Ido Guzi <idoguzi@hotmail.com>
2023-01-16 20:45:00 +0200

    deleting unused commented code

--- vdrm_driver.c
+++ vdrm_driver.c
@@ -25,25 +25,8 @@ struct drm_driver vdrm_drv = {
 
 
 struct vdrm_driver *vdrm_driver_init(struct device *parent) {
-	//int err=0;
 	struct device *dev;
 	struct vdrm_driver *drv;
-	// drv = (struct vdrm_driver *)kzalloc(sizeof(struct vdrm_driver), GFP_KERNEL);
-	// if (drv == NULL) {
-	// 	return NULL;
-	// }
-	// drv->drm_drv = &vdrm_drv;
-
-	// dev = (struct device *)kzalloc(sizeof(struct device), GFP_KERNEL);
-	// if (IS_ERR(dev)) {
-	// 	printk("failed to create dummy parent device, what: %li\n", PTR_ERR(dev));
-	// 	return NULL;
-	// }
-	// err = device_register(dev);
-	// if (err) {
-	// 	printk("failed to register dummy parent device, what: %d\n", err);
-	// 	return NULL;
-	// }
 
 	//devm_drm_dev_alloc (and any other drm dev alloc function) will fail without parent device
 	drv = devm_drm_dev_alloc(parent, &vdrm_drv, struct vdrm_driver, drm_dev);
